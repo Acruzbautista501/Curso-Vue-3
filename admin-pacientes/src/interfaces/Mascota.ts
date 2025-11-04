65c6f6bfb5d65b2c1a3dd2f7ffac2c84320e3238
Acruzbautista501 <aldair.cruzbautista@outlook.com>
2025-11-04 07:13:28 -0600

    Creacion de las interfaces en Admin Mascotas

---
+++ admin-pacientes/src/interfaces/Mascota.ts
@@ -0,0 +1,12 @@
+export interface Mascota {
+    nombre: string
+    propietario: string
+    email: string
+    alta: string
+    sintomas: string
+}
+
+export interface Alertas {
+    tipo: string
+    mensaje: string
+}
