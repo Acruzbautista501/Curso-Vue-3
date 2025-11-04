bf912d1e641c8101df212dd2d02b81d7d21fbdf6
Acruzbautista501 <aldair.cruzbautista@outlook.com>
2025-11-04 07:16:56 -0600

    Creacion de alertas en Admin Mascotas

---
+++ admin-pacientes/src/components/Alerta.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { Alertas } from '../interfaces/Mascota';
+
+  defineProps<{
+    alerta: Alertas
+  }>()
+  
+</script>
+
+<template>
+  <div class="text-white text-center p-3 uppercase font-bold mb-3 rounded-md bg-red-500">
+    {{ alerta.mensaje }}
+  </div>
+</template>
