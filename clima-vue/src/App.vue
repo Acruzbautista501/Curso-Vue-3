f4a5a1c17994b24f478ea9581bf9bc8683679f81
Acruzbautista501 <aldair.cruzbautista@outlook.com>
2025-11-11 07:48:52 -0600

    Creacion de Clima Vue

---
+++ clima-vue/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Formulario from './components/Formulario.vue';
+
+</script>
+
+<template>
+  <h1 class="titulo">Buscador de clima</h1>
+  <div class="contenedor buscador-clima">
+      <Formulario />
+  </div>
+</template>
