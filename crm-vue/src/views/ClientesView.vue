0775e6fc42f4c8a6b01d7f6735556a270da71172
Acruzbautista501 <aldair.cruzbautista@outlook.com>
2025-11-14 07:46:03 -0600

    Creacion de formulario

---
+++ crm-vue/src/views/ClientesView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import RouterLink from '@/components/UI/RouterLink.vue';
+import Heading from '@/components/UI/UiHeading.vue';
+
+defineProps<{
+  titulo: string
+}>()
+</script>
+
+<template>
+  <div>
+    <div class="flex justify-end">
+      <RouterLink to="agregar-cliente">
+        Agregar Cliente
+      </RouterLink>
+    </div>
+    <Heading>{{ titulo }}</Heading>
+  </div>
+</template>
---
+++ crm-vue/src/views/NuevoClienteView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { FormKit } from '@formkit/vue';
+  import RouterLink from '@/components/UI/RouterLink.vue';
+  import Heading from '@/components/UI/UiHeading.vue';
+
+  defineProps<{
+    titulo: string
+  }>()
+</script>
+
+<template>
+  <div>
+    <div class="flex justify-end">
+      <RouterLink to="inicio">
+        Volver
+      </RouterLink>
+    </div>    
+    <Heading>{{ titulo }}</Heading>
+    <div class="mx-auto mt-10 bg-white shadow">
+      <FormKit
+        type="form"
+      >
+      </FormKit>
+    </div>
+  </div>
+</template>
